304d624d75e63407dbfb1a781a32178e1ff6fbd1
Chintan Shah <chintan@wohlig.com>
2017-01-31 16:31:34 +0530

    smaaash pdf

--- api/controllers/UploadController.js
+++ api/controllers/UploadController.js
@@ -35,5 +35,10 @@ module.exports = {
   },
   readFile: function (req, res) {
     Config.readUploaded(req.query.file, req.query.width, req.query.height, req.query.style, res);
-  }
+  },
+  SMAAASH: function (req, res) {
+    Config.readUploaded(req.query.file, req.query.width, req.query.height, req.query.style, res);
+  },
+  
 };
+
