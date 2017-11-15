8da2902ddaee37e4414be16917a37fef542acc89
Chintan <chintan@wohlig.com>
2017-11-15 12:01:35 +0530

    MMM

--- api/controllers/UploadController.js
+++ api/controllers/UploadController.js
@@ -14,16 +14,25 @@ module.exports = {
     req.file("file").upload({
       maxBytes: 50000000 // 10 MB Storage 1 MB = 10^6
     }, function (err, uploadedFile) {
+
       if (uploadedFile && uploadedFile.length > 0) {
         async.each(uploadedFile, function (n, callback) {
-          Config.uploadFile(n.fd, function (err, value) {
-            if (err) {
-              callback(err);
+          if (n.filename) {
+            var splitVal = n.filename.split(".");
+            var extension = splitVal[splitVal.length - 1];
+            if (extension == "jpeg" || extension == "png" || extension == "jpg" || extension == "gif" || extension == "svg" || extension == "pdf" || extension == "xls" || extension == "xlsx" || extension == "doc") {
+              Config.uploadFile(n.fd, function (err, value) {
+                if (err) {
+                  callback(err);
+                } else {
+                  fileNames.push(value.name);
+                  callback(null);
+                }
+              });
             } else {
-              fileNames.push(value.name);
               callback(null);
             }
-          });
+          }
         }, callback2);
       } else {
         callback2(null, {
@@ -39,6 +48,5 @@ module.exports = {
   SMAAASH: function (req, res) {
     Config.readUploaded(req.query.file, req.query.width, req.query.height, req.query.style, res);
   },
-  
-};
 
+};
